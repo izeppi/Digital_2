f8d4ef4444c534d8fac84c11e7edd29000fabd5f
izeppi <ize17229@uvg.edu.gt>
2020-02-15 03:28:49 -0600

    Update SPI.c

--- Lab_4_SPI/Lab_4_SPI.X/SPI.c
+++ Lab_4_SPI/Lab_4_SPI.X/SPI.c
@@ -10,12 +10,18 @@
 #include <stdint.h>
 
 
-void init_SPI (uint8_t selector){
+void init_SPI (uint8_t selector, uint8_t sck_bit ){
     SSPSTATbits.SMP = 0;
     SSPSTATbits.CKE = 1;
     SSPCONbits.SSPEN = 1;
     SSPCONbits.CKP = 1;
-    SSPCONbits.SSPM = selector;   // Master = 0000  Slave = 0100
-            
+    SSPCONbits.SSPM = selector;   // Master = 0000  Slave = 0101
+    TRISCbits.TRISC5 = 0;
+    TRISCbits.TRISC3 = sck_bit;   // Master = 0    Slave = 1
          
 }  
+
+void Envio_SPI (uint8_t Valor){
+    while (PIR1bits.SSPIF == 0){}
+    
+}
