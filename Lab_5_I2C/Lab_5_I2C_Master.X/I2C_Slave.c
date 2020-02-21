eaf30f1fa881bae06a68192a46f9ee21ad636614
izeppi <ize17229@uvg.edu.gt>
2020-02-21 11:45:59 -0600

    Laboratorio 5 I2C
    creacion del laboratorio y plantilla.

---
+++ Lab_5_I2C/Lab_5_I2C_Master.X/I2C_Slave.c
@@ -0,0 +1,13 @@
+/*
+ * File:   I2C_Slave.c
+ * Author: Marco
+ *
+ * Created on 21 de febrero de 2020, 11:43 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
---
+++ Lab_5_I2C/Lab_5_I2C_Slave.X/I2C_Master.c
@@ -0,0 +1,13 @@
+/*
+ * File:   I2C_Master.c
+ * Author: Marco
+ *
+ * Created on 21 de febrero de 2020, 11:43 AM
+ */
+
+
+#include <xc.h>
+
+void main(void) {
+    return;
+}
